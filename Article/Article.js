165e44ecc39adccba34ee71aaabbdf64019a8702
Blake Goms <blakegoms@gmail.com>
2019-05-15 15:24:40 -0700

    CSS Stretch

--- Article/Article.js
+++ Article/Article.js
@@ -15,8 +15,7 @@ class Article {
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.article.classList.toggle("article-open");
-    
+    this.article.classList.toggle("article-open");    
   }
 }
 
